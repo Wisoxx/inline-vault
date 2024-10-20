44b4f77310d6e4b5c9ab37b104ac9a6882592568
Wisoxx <bohdantavanov@gmail.com>
2024-10-20 18:57:47 +0200

    Finished database structure

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 import time
+
 from logger import setup_logger
 
 MAX_RETRIES = 3
@@ -27,7 +28,7 @@ class Connection:
 class Database:
     table_name = ""
     create_table_query = ""
-    columns = []
+    columns = ()
 
     connection = Connection('data.db', timeout=5)
 
@@ -50,7 +51,7 @@ class Database:
                 if not query.strip().upper().startswith("SELECT"):
                     connection.commit()
                     logger.debug(f"Query executed successfully")
-                    return cursor.rowcount
+                    return cursor
                 else:
                     results = cursor.fetchall()  # Return results for SELECT statements
                     logger.debug(f"Query executed successfully. Results: {results}")
@@ -108,7 +109,7 @@ class Database:
         cls.execute_query(cls.create_table_query)
 
     @classmethod
-    def add(cls, data: dict, replace: bool = False) -> bool:
+    def add(cls, data: dict, replace: bool = False) -> tuple[bool, int]:
         """
         Inserts a new record into the table. Column names and values are passed as a
         dictionary. Optionally, use "INSERT OR REPLACE" to handle unique constraint
@@ -130,11 +131,13 @@ class Database:
         else:
             query = f"INSERT INTO {cls.table_name} ({columns}) VALUES ({placeholders})"
 
-        return cls.execute_query(query, data.values()) > 0
+        cursor = cls.execute_query(query, data.values())
+
+        return cursor.rowcount > 0, cursor.lastrowid
 
     @classmethod
     def get(cls, conditions: dict = None, limit: int = None, offset: int = None,
-            order_by: str = None, sort_direction: str = 'ASC', include_column_names=False) -> list or dict or tuple:
+            order_by: str = None, sort_direction: str = 'ASC', include_column_names=False, custom_select=None) -> list or dict or tuple:
         """
         Fetch records from the database with optional conditions, limit, offset, and ordering.
 
@@ -144,10 +147,11 @@ class Database:
         :param order_by: The column to order by (optional)
         :param sort_direction: 'ASC' or 'DESC' to define sorting direction (default: 'ASC')
         :param include_column_names: Whether to return column names with values (default: False)
+        :param custom_select: A custom SELECT query to override the default (optional)
         :return: List of fetched records
         """
 
-        query = f"SELECT * FROM {cls.table_name}"
+        query = custom_select if custom_select else "SELECT * FROM {cls.table_name}"
         params = []
 
         # Add WHERE conditions if provided
@@ -167,11 +171,11 @@ class Database:
             query += f" ORDER BY {order_by} {sort_direction.upper()}"
 
         # Add LIMIT and OFFSET if provided
-        if limit is not None:
+        if limit:
             query += " LIMIT ?"
             params.append(limit)
 
-        if offset is not None:
+        if offset:
             query += " OFFSET ?"
             params.append(offset)
 
@@ -211,7 +215,8 @@ class Database:
         set_clause = ', '.join(f"{key} = ?" for key in new_values)
         where_clause = ' AND '.join(f"{key} = ?" for key in conditions)
         query = f"UPDATE {cls.table_name} SET {set_clause} WHERE {where_clause}"
-        return cls.execute_query(query, (*new_values.values(), *conditions.values())) > 0
+        cursor = cls.execute_query(query, (*new_values.values(), *conditions.values()))
+        return cursor.rowcount > 0
 
     @classmethod
     def delete(cls, conditions: dict):
@@ -222,77 +227,187 @@ class Database:
 
         where_clause = ' AND '.join(f"{key} = ?" for key in conditions)
         query = f"DELETE FROM {cls.table_name} WHERE {where_clause}"
-        return cls.execute_query(query, tuple(conditions.values())) > 0
+        cursor = cls.execute_query(query, tuple(conditions.values()))
+        return cursor.rowcount > 0
 
 
 class Users(Database):
     table_name = "users"
-    columns = ["user_id", "username", "language", "timezone", "current_vocabulary_id", "hide_meaning"]
+    columns = ["user_id", "username"]
     create_table_query = '''
     CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY,
-    username TEXT NOT NULL,
-    language TEXT NOT NULL,
-    timezone INTEGER,
-    current_vocabulary_id INTEGER,
-    hide_meaning BOOLEAN NOT NULL DEFAULT 1,
-    FOREIGN KEY (current_vocabulary_id) REFERENCES vocabularies(vocabulary_id) ON DELETE SET NULL
+        user_id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL
     );
     '''
 
 
-class Vocabularies(Database):
-    table_name = "vocabularies"
-    columns = ["vocabulary_id", "user_id", "vocabulary_name"]
-    create_table_query = """
-    CREATE TABLE IF NOT EXISTS vocabularies (
-    vocabulary_id INTEGER PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    vocabulary_name TEXT NOT NULL,
-    UNIQUE(vocabulary_name, user_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
-    );
-    """
+class Media(Database):
+    table_name = "media"
+    columns = ("user_id", "media_type", "file_id", "caption", "media_id", "description")
 
+    @classmethod
+    def create_table(cls) -> None:
+        create_main_table_query = """
+            CREATE TABLE IF NOT EXISTS media (
+                user_id INTEGER NOT NULL,
+                media_type TEXT NOT NULL,
+                file_id TEXT NOT NULL UNIQUE,
+                caption TEXT,
+                media_id INTEGER PRIMARY KEY AUTOINCREMENT,
+                FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+            );
+        """
+        create_table_fts_query = """
+        CREATE VIRTUAL TABLE IF NOT EXISTS media_fts USING fts5(
+            media_id,
+            description
+        );
+        """
+        cls.execute_query(create_main_table_query)
+        cls.execute_query(create_table_fts_query)
 
-class Words(Database):
-    table_name = "words"
-    columns = ["word_id", "user_id", "vocabulary_id", "word", "meaning", "timestamp"]
-    create_table_query = '''
-    CREATE TABLE IF NOT EXISTS words (
-    word_id INTEGER PRIMARY KEY,
-    user_id INTEGER NOT NULL,
-    vocabulary_id INTEGER NOT NULL,
-    word TEXT NOT NULL,
-    meaning TEXT,
-    timestamp INTEGER NOT NULL,
-    UNIQUE(word, user_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-    FOREIGN KEY (vocabulary_id) REFERENCES vocabularies(vocabulary_id) ON DELETE CASCADE
-    );
-    '''
+    @classmethod
+    def add(cls, data: dict, replace: bool = False) -> tuple[bool, int]:
+        media_data = {
+            'user_id': data["user_id"],
+            'media_type': data["media_type"],
+            'file_id': data["file_id"],
+            'caption': data.get("caption", None),
+        }
+        description = data["description"]
 
+        [status, media_id] = super().add(media_data)
+        if status:
+            cls.execute_query('INSERT INTO media_fts (media_id, description) VALUES (?, ?);', (media_id, description))
+        return status, media_id
 
-class Reminders(Database):
-    table_name = "reminders"
-    columns = ["reminder_id", "user_id", "vocabulary_id", "time", "number_of_words"]
-    create_table_query = '''
-    CREATE TABLE IF NOT EXISTS reminders (
-        reminder_id INTEGER PRIMARY KEY,
-        user_id INTEGER NOT NULL,
-        vocabulary_id INTEGER NOT NULL,
-        time TEXT NOT NULL,
-        number_of_words INTEGER NOT NULL,
-        UNIQUE(user_id, time),
-        FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
-        FOREIGN KEY (vocabulary_id) REFERENCES vocabularies(vocabulary_id) ON DELETE CASCADE
-    );
-    '''
+    @classmethod
+    def get(cls, conditions: dict = None, limit: int = None, offset: int = None,
+            order_by: str = None, sort_direction: str = 'ASC', include_column_names=False, custom_select=None) -> list or dict or tuple:
+        joined_select = """
+        SELECT media.*, media_fts.description 
+        FROM media 
+        LEFT JOIN media_fts ON media.media_id = media_fts.media_id
+        """
+        custom_select = custom_select or joined_select
+        return super().get(conditions, limit, offset, order_by, sort_direction, include_column_names, custom_select)
+
+    @classmethod
+    def delete(cls, conditions: dict):
+        """Deletes records that meet the given conditions from both media and media_fts tables."""
+        if not conditions:
+            raise ValueError("No conditions provided for identifying the row(s) to delete.")
+
+        if 'description' in conditions:
+            raise NotImplementedError("Deletion by description is not supported.")
+
+        cls.validate_columns(conditions)
+
+        where_clause = ' AND '.join(f"{key} = ?" for key in conditions)
+
+        delete_fts_query = f"""
+            DELETE FROM media_fts
+            WHERE media_id IN (
+                SELECT media_id FROM media 
+                WHERE {where_clause}
+            );
+        """
+        delete_media_query = f"""
+            DELETE FROM media
+            WHERE {where_clause};
+        """
+
+        params = tuple(conditions.values())
+
+        cls.execute_query(delete_fts_query, params)
+        cursor_media = cls.execute_query(delete_media_query, params)
+
+        return cursor_media.rowcount > 0
+
+    @classmethod
+    def search_by_description(cls, user_id: int, description: str, limit: int = None, offset: int = None) -> tuple:
+        """
+        Searches media by description using Full-Text Search (FTS).
+
+        :param user_id: The ID of the user whose media to search
+        :param description: The text to search in the description
+        :param limit: Maximum number of records to retrieve (optional)
+        :param offset: Number of records to skip (optional)
+        :return: A tuple containing the list of media entries and the total count of records
+        """
+        params = [user_id]
+
+        if description:
+            # Split the description into individual search terms
+            search_terms = description.split()
+            # Create a search string with terms combined by OR
+            search_query = ' OR '.join(search_terms)
+
+            # Query to select media matching the search description
+            query = f"""
+                SELECT media.*, media_fts.description 
+                FROM media
+                JOIN media_fts ON media.media_id = media_fts.media_id
+                WHERE media_fts.description MATCH ? AND media.user_id = ?
+                ORDER BY rank
+            """
+            params.insert(0, search_query)
+
+            # Count query to get total matching records including media_fts
+            count_query = f"""
+                SELECT COUNT(*) 
+                FROM media
+                JOIN media_fts ON media.media_id = media_fts.media_id
+                WHERE media_fts.description MATCH ? AND media.user_id = ?
+            """
+            total_count_params = [search_query, user_id]
+
+        else:  # If description is empty, fetch all records for the user
+            # Query to select all media for the specified user
+            query = f"""
+                SELECT media.*, media_fts.description 
+                FROM media
+                JOIN media_fts ON media.media_id = media_fts.media_id
+                WHERE media.user_id = ?
+                ORDER BY rank
+            """
+
+            # Count query for total records for the user
+            count_query = """
+                SELECT COUNT(*) 
+                FROM media
+                WHERE user_id = ?
+            """
+            total_count_params = [user_id]
+
+        # Execute the count query to get total number of records
+        total_count = cls.execute_query(count_query, total_count_params)[0][0]
+
+        if limit:
+            # Add LIMIT clause if a limit is specified
+            query += " LIMIT ?"
+            params.append(limit)
+
+        if offset:
+            if limit:
+                # Add OFFSET clause if both limit and offset are specified
+                query += " OFFSET ?"
+                params.append(offset)
+            else:
+                # Raise an error if offset is specified without limit
+                raise ValueError("Offset cannot be specified without a limit.")
+
+        # Execute the main query to fetch media records
+        records = cls.execute_query(query, params)
+
+        # Return both the fetched records and the total count of records
+        return records, total_count
 
 
 class Temp(Database):
     table_name = "temp"
-    columns = ["user_id", "key", "value"]
+    columns = ("user_id", "key", "value")
     create_table_query = """
     CREATE TABLE IF NOT EXISTS temp (
         user_id INTEGER,
@@ -304,5 +419,5 @@ class Temp(Database):
     """
 
     @classmethod
-    def add(cls, data: dict, replace: bool = True):
+    def add(cls, data: dict, replace: bool = True) -> tuple[bool, int]:
         return super().add(data, replace=replace)
