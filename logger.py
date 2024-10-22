1bd94af2fe9768c0860a335ba35a8ef816278c42
Wisoxx <bohdantavanov@gmail.com>
2024-10-22 18:40:05 +0200

    fixed having each log entry duplicated

--- logger.py
+++ logger.py
@@ -7,21 +7,20 @@ def setup_logger(name):
     log_path = os.path.join(os.path.expanduser("~"), 'mysite', 'logs')
     os.makedirs(log_path, exist_ok=True)
 
-    file_handler = RotatingFileHandler(os.path.join(log_path, 'app.log'), maxBytes=1000000, backupCount=4)
-    formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
-                                  datefmt='%Y-%m-%d %H:%M:%S')
-    file_handler.setFormatter(formatter)
-
-    console_handler = logging.StreamHandler()
-    console_formatter = logging.Formatter('%(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    console_handler.setFormatter(console_formatter)
-
     logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
 
-    if logger.hasHandlers():
-        logger.handlers.clear()
-    logger.addHandler(file_handler)
-    logger.addHandler(console_handler)
+    if not logger.hasHandlers():
+        file_handler = RotatingFileHandler(os.path.join(log_path, 'app.log'), maxBytes=1000000, backupCount=4)
+        formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
+                                      datefmt='%Y-%m-%d %H:%M:%S')
+        file_handler.setFormatter(formatter)
+
+        console_handler = logging.StreamHandler()
+        console_formatter = logging.Formatter('%(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+        console_handler.setFormatter(console_formatter)
+
+        logger.setLevel(logging.INFO)
+        logger.addHandler(file_handler)
+        logger.addHandler(console_handler)
 
     return logger
