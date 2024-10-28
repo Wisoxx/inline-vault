3f2d0a41a64a4987942b5419b7ea05f7c241298c
Wisoxx <bohdantavanov@gmail.com>
2024-10-28 20:26:36 +0100

    console logger is set to debug

--- logger.py
+++ logger.py
@@ -14,12 +14,14 @@ def setup_logger(name):
         formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
                                       datefmt='%Y-%m-%d %H:%M:%S')
         file_handler.setFormatter(formatter)
+        file_handler.setLevel(logging.INFO)
 
         console_handler = logging.StreamHandler()
         console_formatter = logging.Formatter('%(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
         console_handler.setFormatter(console_formatter)
+        console_handler.setLevel(logging.DEBUG)
 
-        logger.setLevel(logging.INFO)
+        logger.setLevel(logging.DEBUG)  # lowest level to allow separate levels for files and console
         logger.addHandler(file_handler)
         logger.addHandler(console_handler)
 
