a0669c0c38febb78878187cdf0798eadf72d9e80
Wisoxx <bohdantavanov@gmail.com>
2024-10-28 22:28:52 +0100

    trying to fix displaying cyrillic in logs try #2

--- logger.py
+++ logger.py
@@ -1,6 +1,8 @@
 import logging
 from logging.handlers import RotatingFileHandler
 import os
+import io
+import sys
 
 
 def setup_logger(name):
@@ -16,7 +18,7 @@ def setup_logger(name):
         file_handler.setFormatter(formatter)
         file_handler.setLevel(logging.INFO)
 
-        console_handler = logging.StreamHandler()
+        console_handler = logging.StreamHandler(io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8'))
         console_formatter = logging.Formatter('%(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
         console_handler.setFormatter(console_formatter)
         console_handler.setLevel(logging.DEBUG)
